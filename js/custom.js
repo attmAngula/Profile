fcb14fca92b7e5dbea11a2976a1d18964a60ce73
Tuhafeni <tmtangula11@gmail.com>
2018-09-28 01:07:07 +0200

    Updated Profile - September 2018

--- js/custom.js
+++ js/custom.js
@@ -1,9 +1,4 @@
 // my JS
 
 // function to get the current year.
-function currentYear(){
-	var date = new Date();
-	var year = date.getFullYear();
-	
-	document.getElementById("year").innerHTML = year;
-}
+function getCurrentYear(){let date=new Date();let year=date.getFullYear();document.getElementById("year").innerHTML=year;}
